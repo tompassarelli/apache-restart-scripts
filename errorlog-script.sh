07609b729726e311fc6d721255fc439e20359270
Tom Passarelli <tom.passarelli@protonmail.com>
2022-11-28 20:59:18 -0500

    drafts

---
+++ errorlog-script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+MYFQDN="www.company.com";
+MYTITLE="company";
+MYHOST="www";
+MYTLD="com";
+MY_APACHE_LOG="/var/log/apache2/company-access.log";
+ADMIN_EMAIL="youremail@example.com"
+DATESTAMP=`date +"%d-%m-%y"`;
+CHECK_LOG=/root/${MYFQDN}-apache-access-log-${DATESTAMP}.log
+ 
+# inspect last 100 logs and parse errors, if > 10% restart server later
+# we want to overwrite the log so we are only keeping track of recent errors
+tail -100 ${MY_APACHE_LOG} | awk '($9 ~ /500/)' | > ${CHECK_LOG};
+tail -100 ${MY_APACHE_LOG} | awk '($9 ~ /503/)' | > ${CHECK_LOG};
+
+ 
+ERRORS = $(cat $CHECK_LOG | tail -10)
+ERROR_COUNT = $($LAST_ERRORS | wc -1)
+if [ $ERRORS gt 10 ] 
+  systemctl restart httpd
+  systemctl restart php-fpm
+  systemctl restart mysqld
+  /bin/mail -s "server halted, server will restart" "you@YourEmailAddress" < textBody.letter
+fi
---
+++ systemctl-script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+HTTP_STATUS = systemctl show httpd | grep StatusText | cut -d "=" -f2
+
+if [ $HTTP_STATUS != Running ]; then
+  systemctl restart httpd
+  systemctl restart php-fpm
+  systemctl restart mysqld
+  /bin/mail -s "server halted, server will restart" "you@YourEmailAddress" < textBody.letter
+fi
