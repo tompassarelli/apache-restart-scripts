a47013333a654ffd30b3adae9420bbcb1b57bb03
Tom Passarelli <tom.passarelli@protonmail.com>
2022-12-03 12:16:24 -0500

    Update systemctl-script.sh
    added cron info and ; to ensure each restart waits to succeed.

--- systemctl-script.sh
+++ systemctl-script.sh
@@ -2,8 +2,15 @@
 HTTP_STATUS = systemctl show httpd | grep StatusText | cut -d "=" -f2
 
 if [ $HTTP_STATUS != "Running" ]; then
-  systemctl restart httpd
-  systemctl restart php-fpm
-  systemctl restart mysqld
-  /bin/mail -s "server halted, server will restart" "you@YourEmailAddress" < textBody.letter
+  systemctl restart httpd;
+  systemctl restart php-fpm;
+  systemctl restart mysqld;
+  #/bin/mail -s "server halted, server will restart" "you@YourEmailAddress" < textBody.letter
 fi
+
+# cron info:
+# first chmod 755 systemctl-script.sh (enable script execute permissions)
+# then
+# run crontab -e
+# enter */10  *  *  *  *  /path/to/systemctl-script.sh (every 10 minutes)
+# if the restart fails, see https://upcloud.com/resources/tutorials/fix-common-problems-apache2
