1174f633a7471112bb1db399ea0bbe5f0a7e02cc
Tom Passarelli <tom.passarelli@protonmail.com>
2022-12-03 12:20:17 -0500

    Update systemctl-script.sh
    one lined the then statement

--- systemctl-script.sh
+++ systemctl-script.sh
@@ -2,9 +2,7 @@
 HTTP_STATUS = systemctl show httpd | grep StatusText | cut -d "=" -f2
 
 if [ $HTTP_STATUS != "Running" ]; then
-  systemctl restart httpd;
-  systemctl restart php-fpm;
-  systemctl restart mysqld;
+  systemctl restart httpd; systemctl restart php-fpm; systemctl restart mysqld
   #/bin/mail -s "server halted, server will restart" "you@YourEmailAddress" < textBody.letter
 fi
 
